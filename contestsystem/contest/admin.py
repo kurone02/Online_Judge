fcab18a340e4ee9b90554a2f6c91e86fce1bbcbf
kurone02 <56928903+kurone02@users.noreply.github.com>
2020-05-22 00:26:15 +0700

    Delete admin.py

--- contestsystem/contest/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Problems
-
-# Register your models here.
-class ProblemsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'time_limit', 'memory_limit', 'max_score']
-    search_fields = ['title']
-
-admin.site.register(Problems, ProblemsAdmin)
